c4e83e4d7ffd21f3b12477ff971f3bd0f7bfd8f3
Ethal07 <141444629+Ethal07@users.noreply.github.com>
2024-07-17 10:39:14 +0000

    Added storage and retrieval functions

---
+++ main.js
@@ -0,0 +1,24 @@
+function storeRecipe(recipeName){
+    listNames = localStorage.getItem("listNames")
+    if (listNames) {
+        listNames = JSON.parse(listNames)
+        listNames.push(recipeName)
+    }
+    else {
+        listNames = [recipeName]
+    }
+    localStorage.setItem("listNames",listNames)
+    retrieveRecipes()
+}
+
+function retrieveRecipes(){
+    listNames = localStorage.getItem("listNames")
+    if (listNames) {
+        document.getElementById("recipeNamesDisplay").value = listNames
+    }
+    else {
+        document.getElementById("recipeNamesDisplay").value = "No recipes found"
+    }
+}
+
+window.onload = retrieveRecipes()
